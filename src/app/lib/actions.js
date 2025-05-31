09ef63361cafa4a8905c25f011fb1ac996b67138
varunsareen81 <varunsareen81@gmail.com>
2025-05-31 08:14:37 +0530

    modify

--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -17,6 +17,7 @@ export async function authenticate(
     if (error instanceof AuthError) {
       switch (error.type) {
         case 'CredentialsSignin':
+        
           return 'Invalid credentials.';
         default:
           return 'Something went wrong.';
--- src/app/ui/login-form.jsx
+++ src/app/ui/login-form.jsx
@@ -6,10 +6,11 @@ ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from '@/app/ui/button';
-import { useFormState, useFormStatus } from 'react-dom';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { authenticate } from '@/app/lib/actions';
 export default function LoginForm() {
-const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+const [errorMessage, dispatch] = useActionState(authenticate, undefined);
 return (
 <form action={dispatch} className="space-y-3">
 <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
